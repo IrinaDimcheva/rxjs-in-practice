6284f0c655b23f97e5d24e6803f270efa1699260
Irina Dimcheva <dimchevairina@gmail.com>
2024-01-25 00:19:33 +0200

    Same streams with rxjs observables

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,26 +1,40 @@
-import { Component, OnInit, ViewEncapsulation } from "@angular/core";
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { fromEvent, interval, timer } from 'rxjs';
 
 @Component({
-  selector: "about",
-  templateUrl: "./about.component.html",
-  styleUrls: ["./about.component.css"],
+  selector: 'about',
+  templateUrl: './about.component.html',
+  styleUrls: ['./about.component.css'],
 })
 export class AboutComponent implements OnInit {
   constructor() {}
 
   ngOnInit() {
-    document.addEventListener("click", (event) => {
-      console.log(event);
-    });
+    // document.addEventListener('click', (event) => {
+    //   console.log(event);
 
-    let counter = 0;
-    setInterval(() => {
-      console.log(counter);
-      counter++;
-    }, 1000);
+    //   setTimeout(() => {
+    //     console.log('finished...');
 
-    setTimeout(() => {
-      console.log("finished...");
-    }, 3000);
+    //     let counter = 0;
+    //     setInterval(() => {
+    //       console.log(counter);
+    //       counter++;
+    //     }, 1000);
+    //   }, 3000);
+    // });
+
+    ///////////////////////////////
+
+    // RxJs Observable
+    // const interval$ = interval(1000);
+    // interval$.subscribe((val) => console.log('stream 1 => ' + val));
+    // interval$.subscribe((val) => console.log('stream 2 => ' + val));
+
+    const interval$ = timer(3000, 1000);
+    interval$.subscribe((val) => console.log('stream 1 => ' + val));
+
+    const click$ = fromEvent(document, 'click');
+    click$.subscribe((event) => console.log(event));
   }
 }
