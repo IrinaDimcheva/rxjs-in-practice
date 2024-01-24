2bb11b7657b48e1a6620d2f06fe20f968ffaa4ee
Irina Dimcheva <dimchevairina@gmail.com>
2024-01-25 00:35:26 +0200

    Core RxJs concepts - errors, completion and subscriptions

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -10,31 +10,22 @@ export class AboutComponent implements OnInit {
   constructor() {}
 
   ngOnInit() {
-    // document.addEventListener('click', (event) => {
-    //   console.log(event);
-
-    //   setTimeout(() => {
-    //     console.log('finished...');
-
-    //     let counter = 0;
-    //     setInterval(() => {
-    //       console.log(counter);
-    //       counter++;
-    //     }, 1000);
-    //   }, 3000);
-    // });
-
-    ///////////////////////////////
-
-    // RxJs Observable
     // const interval$ = interval(1000);
     // interval$.subscribe((val) => console.log('stream 1 => ' + val));
     // interval$.subscribe((val) => console.log('stream 2 => ' + val));
 
     const interval$ = timer(3000, 1000);
-    interval$.subscribe((val) => console.log('stream 1 => ' + val));
+    const subscription = interval$.subscribe((val) =>
+      console.log('stream 1 => ' + val)
+    );
+
+    setTimeout(() => subscription.unsubscribe(), 5000);
 
     const click$ = fromEvent(document, 'click');
-    click$.subscribe((event) => console.log(event));
+    click$.subscribe(
+      (event) => console.log(event),
+      (err) => console.log(err),
+      () => console.log('completed')
+    );
   }
 }
