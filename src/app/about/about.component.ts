6d46fac6ac09fc5de633e55865846646a5ef4514
Irina Dimcheva <dimchevairina@gmail.com>
2024-01-25 15:10:29 +0200

    Form draft pre-save example

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { concat, of } from 'rxjs';
 
 @Component({
   selector: 'about',
@@ -7,14 +6,5 @@ import { concat, of } from 'rxjs';
   styleUrls: ['./about.component.css'],
 })
 export class AboutComponent implements OnInit {
-  ngOnInit() {
-    // If source1$ = interval(1000); it will never complete
-    const source1$ = of(1, 2, 3);
-    const source2$ = of(4, 5, 6);
-    const source3$ = of(7, 8, 9);
-    const result$ = concat(source1$, source2$, source3$);
-
-    // result$.subscribe((val) => console.log(val));
-    result$.subscribe(console.log);
-  }
+  ngOnInit() {}
 }
