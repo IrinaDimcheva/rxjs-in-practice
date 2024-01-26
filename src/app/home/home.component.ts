dd47ec7134ce9727c906a8943066df66459c2c64
Irina Dimcheva <dimchevairina@gmail.com>
2024-01-26 12:56:58 +0200

    Error handling - catch and rethrow + finalize

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable, of } from 'rxjs';
-import { catchError, map, shareReplay, tap } from 'rxjs/operators';
+import { Observable, of, throwError } from 'rxjs';
+import { catchError, finalize, map, shareReplay, tap } from 'rxjs/operators';
 
 import { createHttpObservable } from '../common/util';
 import { Course } from '../model/course';
@@ -17,25 +17,16 @@ export class HomeComponent implements OnInit {
   ngOnInit() {
     const http$ = createHttpObservable('/api/courses');
     const courses$: Observable<Course[]> = http$.pipe(
+      catchError((err) => {
+        console.log('Error occurred', err);
+        return throwError(err);
+      }),
+      finalize(() => {
+        console.log('Finalize executed...');
+      }),
       tap(() => console.log('HTTP request executed')),
       map((res) => Object.values(res['payload'])),
-      shareReplay(),
-      catchError((err) =>
-        of([
-          {
-            id: 0,
-            description: 'RxJs In Practice Course',
-            iconUrl:
-              'https://s3-us-west-1.amazonaws.com/angular-university/course-images/rxjs-in-practice-course.png',
-            courseListIcon:
-              'https://angular-academy.s3.amazonaws.com/main-logo/main-page-logo-small-hat.png',
-            longDescription:
-              'Understand the RxJs Observable pattern, learn the RxJs Operators via practical examples',
-            category: 'BEGINNER',
-            lessonsCount: 10,
-          },
-        ])
-      )
+      shareReplay()
     ) as Observable<Course[]>;
 
     this.beginnersCourses$ = courses$.pipe(
