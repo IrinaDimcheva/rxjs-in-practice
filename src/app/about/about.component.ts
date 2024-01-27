491f80a3af6cc484e1509561e579f169ccd99070
Irina Dimcheva <dimchevairina@gmail.com>
2024-01-27 10:18:09 +0200

    Example of subject

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'about',
@@ -6,5 +7,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about.component.css'],
 })
 export class AboutComponent implements OnInit {
-  ngOnInit() {}
+  ngOnInit() {
+    // const subject = new Subject();
+    // const series$ = subject.asObservable();
+    // series$.subscribe(console.log);
+
+    // subject.next(1);
+    // subject.next(2);
+    // subject.next(3);
+    // subject.complete();
+
+    const subject = new Subject();
+    const series$ = subject.asObservable();
+    series$.subscribe((val) => console.log('early sub: ' + val));
+
+    subject.next(1);
+    subject.next(2);
+    subject.next(3);
+
+    setTimeout(() => {
+      series$.subscribe((val) => console.log('late sub: ' + val));
+
+      subject.next(4);
+    }, 3000);
+  }
 }
