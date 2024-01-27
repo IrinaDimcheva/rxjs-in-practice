bfcf656b95eff2f197e937142ce91f3e6d1cea1e
Irina Dimcheva <dimchevairina@gmail.com>
2024-01-27 10:39:08 +0200

    BehaviorSubject example

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Subject } from 'rxjs';
+import { BehaviorSubject, Subject } from 'rxjs';
 
 @Component({
   selector: 'about',
@@ -8,22 +8,15 @@ import { Subject } from 'rxjs';
 })
 export class AboutComponent implements OnInit {
   ngOnInit() {
-    // const subject = new Subject();
-    // const series$ = subject.asObservable();
-    // series$.subscribe(console.log);
-
-    // subject.next(1);
-    // subject.next(2);
-    // subject.next(3);
-    // subject.complete();
-
-    const subject = new Subject();
+    const subject = new BehaviorSubject(0);
     const series$ = subject.asObservable();
     series$.subscribe((val) => console.log('early sub: ' + val));
 
     subject.next(1);
     subject.next(2);
     subject.next(3);
+
+    // subject.complete();
 
     setTimeout(() => {
       series$.subscribe((val) => console.log('late sub: ' + val));
